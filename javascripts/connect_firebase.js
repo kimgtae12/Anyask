61f2820fa488f9e5eeef0924aeb335effbc0b725
kimgtae12 <kimgtea@naver.com>
2021-05-14 15:55:37 +0900

    join commit

---
+++ javascripts/connect_firebase.js
@@ -0,0 +1,18 @@
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAausUoaQvD2SePUEmt1ucav5eiqP2yJPw",
+    authDomain: "anyask-a1a5f.firebaseapp.com",
+    databaseURL: "https://anyask-a1a5f-default-rtdb.firebaseio.com",
+    projectId: "anyask-a1a5f",
+    storageBucket: "anyask-a1a5f.appspot.com",
+    messagingSenderId: "913453891807",
+    appId: "1:913453891807:web:05e67d5ada2e8229a7a96a",
+    measurementId: "G-Q2FRTYGT7C"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
